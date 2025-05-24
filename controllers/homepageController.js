92aa289baca758dc0814e7676ee7563f9f5b4111
JobertDev27 <jobertdev27@gmail.com>
2025-05-24 16:30:49 +0800

    added a search function and item page display

--- controllers/homepageController.js
+++ controllers/homepageController.js
@@ -1,7 +1,17 @@
 import query from "../data/query.js";
 
 export async function homepage(req, res) {
+  const { search } = req.query;
+  if (search) {
+    const searchData = await query.getQueryWeapons(search);
+    return res.render("home", { title: "Search Result", weapons: searchData });
+  }
   const weaponsData = await query.getAllWeapons();
-  console.log(query.getAllWeapons);
   res.render("home", { title: "Homepage", weapons: weaponsData });
+}
+
+export async function weaponPage(req, res) {
+  const weaponId = req.params.weaponId;
+  const queryWeapon = await query.getWeaponsById(weaponId);
+  res.render("itemPage", { weapon: queryWeapon[0] });
 }
