b0e6b3191839af298a2c9763f2a5d7f7cc3a8098
JobertDev27 <jobertdev27@gmail.com>
2025-05-25 16:16:54 +0800

    added post router

--- routes/homepageRouter.js
+++ routes/homepageRouter.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { homepage, weaponPage } from "../controllers/homepageController.js";
+import {
+  homepage,
+  weaponPage,
+  weapondPagePost,
+} from "../controllers/homepageController.js";
 
 const route = Router();
 
 route.get("/", homepage);
 route.get("/:weaponId", weaponPage);
-
+route.post(":weaponId", weapondPagePost);
 export default route;
