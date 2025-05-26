d32c759ee403671f2779da8d7b468ef87f959fd3
JobertDev27 <jobertdev27@gmail.com>
2025-05-26 14:00:33 +0800

    remove post dependency

--- routes/homepageRouter.js
+++ routes/homepageRouter.js
@@ -1,13 +1,9 @@
 import { Router } from "express";
-import {
-  homepage,
-  weaponPage,
-  weapondPagePost,
-} from "../controllers/homepageController.js";
+import { homepage, weaponPage } from "../controllers/homepageController.js";
 
 const route = Router();
 
 route.get("/", homepage);
 route.get("/:weaponId", weaponPage);
-route.post(":weaponId", weapondPagePost);
+// route.post(":weaponId", weapondPagePost);
 export default route;
